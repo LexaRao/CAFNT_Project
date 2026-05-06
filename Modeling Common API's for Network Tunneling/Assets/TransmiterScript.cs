f4daba0dbdaa3cd9998657f22ef3e960d8208b3d
LexaRao <lrao1160@sdsu.edu>
2026-05-06 10:13:59 -0700

    Added Software: Project Model.
    Added the project model, which can include different variables.  This can allow for live demonstrations of the prototype in use.

---
+++ Modeling Common API's for Network Tunneling/Assets/TransmiterScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class TransmiterScript : MonoBehaviour
+{
+    // Define the reciever for this program.
+    public GameObject transmissionReceiver;
+    int powerlevels = 0;
+    bool signelPresent = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Gather the game objects of both the transmittor and reciever.
+
+
+        // Initialize both the transmittor and the reciever to off.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When the transmittor is set to the right strength change the indicator for two ms.
+        
+        // Wait 4 ms before change the reciever and transmittor.
+        // wait(4);
+
+        // If power is correct level then allow the reciever to flicker.
+        if (powerlevels >= 5) {
+            // When the transmittor is able to recieve the data change it indicator light and transmitters.
+            
+            // Wait for ms.
+            // wait(4);
+            
+            // Flip reciever back off.
+            
+        }
+
+        // Change everything back to neteral before next single.
+
+    }
+}
---
+++ Modeling Common API's for Network Tunneling/Library/PackageCache/com.unity.toolchain.macos-x86_64-linux@6f1d7b3cfeeb/README.md
@@ -0,0 +1,3 @@
+# com.unity.toolchain.macos-x86_64-linux
+
+This package provides a clang toolchain for building Linux and Embedded Linux IL2CPP players on a MacOS x86_64 host.
